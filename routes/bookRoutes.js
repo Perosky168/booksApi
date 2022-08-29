079ea26a60371181d006873e260903eb33f91bf8
unknown <ogunbanjopetet10@gmail.com>
2022-08-29 13:18:43 +0100

    made minor changes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,11 +1,22 @@
 const express = require('express');
 
 const bookController = require('../controllers/bookController');
+const authController = require('../controllers/authController');
+const reviewRouter = require('../routes/reviewRoutes');
 
 const router = express.Router();
 
-router.get('/', bookController.getAllBooks);
-router.post('/', bookController.createBook);
-router.patch('/:id', bookController.updateBook);
+router.use('/:bookId/reviews', reviewRouter);
+
+router
+    .route('/')
+    .get(authController.protect, bookController.getAllBooks)
+    .post(bookController.createBook);
+
+router
+    .route('/:id')
+    .get(bookController.getBook)
+    .patch(bookController.updateBook)
+    .delete(bookController.deleteBook);
 
 module.exports = router;
